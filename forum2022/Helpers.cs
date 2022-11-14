8e488925e5c98379b342a1477fb9e9e02f7b7bf2
websorokinweb <bohdansorokin7@gmail.com>
2022-11-14 08:30:16 +0100

    Added Helpers + Menu init

---
+++ forum2022/Helpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forum2022
+{
+    internal class Helpers
+    {
+        public double SaveUserDouble(string message, string errorMessage)
+        {
+            double correctUserInput;
+            string userInput;
+
+            Console.Write(message + " ");
+            userInput = Console.ReadLine();
+            while (!Double.TryParse(userInput, out correctUserInput))
+            {
+                Console.Write(errorMessage + " ");
+                userInput = Console.ReadLine();
+            }
+            return correctUserInput;
+        }
+
+        public int SaveUserInt(string message, string errorMessage)
+        {
+            int correctUserInput;
+            string userInput;
+
+            Console.Write(message + " ");
+            userInput = Console.ReadLine();
+            while (!int.TryParse(userInput, out correctUserInput))
+            {
+                Console.Write(errorMessage + " ");
+                userInput = Console.ReadLine();
+            }
+            return correctUserInput;
+        }
+
+        public string SaveUserStr(string message, string errorMessage)
+        {
+            string userInput;
+
+            Console.Write(message + " ");
+            userInput = Console.ReadLine();
+            return userInput;
+        }
+    }
+}
---
+++ forum2022/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forum2022
+{
+    internal class Menu
+    {
+        public bool isActive = true;
+
+        public void initMenu()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
